2faf787c51e940580bae705c58a108eb3177b108
Adam Leuer <aleuer2@illinois.edu>
2021-01-13 20:17:45 -0800

    Add project files.

---
+++ GHelper/GHelper/Utility/GHubItemTemplateSelector.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using GHelper.Models;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace GHelper.Utility
+{
+	public class GHubItemTemplateSelector : DataTemplateSelector
+	{
+		public DataTemplate ContextTemplate { get; set; }
+		public DataTemplate ProfileTemplate { get; set; }
+
+		protected override DataTemplate SelectTemplateCore(object item)
+		{
+			if (item is Context)
+			{
+				return ContextTemplate;
+			}
+			else if (item is Profile)
+			{
+				return ProfileTemplate;
+			}
+			else
+			{
+				throw new ArgumentException("Unknown type of item passed to GHubItemTemplateSelector");
+			}
+		}
+	}
+}
---
+++ GHelper/GHelper/View/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+using Windows.ApplicationModel.Core;
+using Windows.UI.ViewManagement;
+using GHelper.Models;
+using Microsoft.UI.Xaml;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace GHelper.View
+{
+	/// <summary>
+	/// An empty window that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class MainWindow : Window
+	{
+		private ObservableCollection<Context> Contexts
+		{
+			get;
+		} = new ObservableCollection<Context>();
+	
+
+		public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+	}
+}
