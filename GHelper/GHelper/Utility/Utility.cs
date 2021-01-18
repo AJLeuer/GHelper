6b1ffb99f7ba4883a4f01ba8e8363f37a19d1512
Adam Leuer <aleuer2@illinois.edu>
2021-01-18 01:19:35 -0800

    Now retrieves context images programatically

---
+++ GHelper/GHelper/Utility/Utility.cs
@@ -0,0 +1,36 @@
+﻿using System.Drawing;
+using System.IO;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media.Imaging;
+using System;
+
+namespace GHelper.Utility
+{
+	public static class Utility
+	{
+		public static async Task<Image> CreateSingleColorImage(Color color, Size size)
+		{
+			var bitmap = new WriteableBitmap(size.Width, size.Height);
+			var pixels = new byte[size.Width * size.Height * 4];
+			
+			for (uint i = 0; i < pixels.Length; i += 4)
+			{
+				//BGRA format
+				pixels[i] = color.B;
+				pixels[i + 1] = color.G;
+				pixels[i + 2] = color.R;
+				pixels[i + 3] = color.A;
+			}
+
+			await using (Stream stream = bitmap.PixelBuffer.AsStream())
+			{
+				//write to bitmap
+				await stream.WriteAsync(pixels.AsMemory(0, pixels.Length));
+			}
+			
+			return new Image { Source = bitmap };
+		}
+	}
+}
--- GHelper/GHelper/ViewModel/ContextView.xaml.cs
+++ GHelper/GHelper/ViewModel/ContextView.xaml.cs
@@ -1,27 +1,64 @@
-﻿using GHelperLogic.Model;
+﻿using System.Drawing;
+using GHelperLogic.Model;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media.Imaging;
+using static GHelper.Utility.Utility;
+using Color = System.Drawing.Color;
 
 namespace GHelper.ViewModel
 {
-    public partial class ContextView : StackPanel
-    {
-	    public static readonly DependencyProperty ContextProperty = DependencyProperty.Register(
-		    nameof (ContextView.Context),
-		    typeof (Context),
-		    typeof (ContextView),
-		    new PropertyMetadata(null)
-	    );
-	    
-	    public Context Context
-	    {
-		    get { return (Context) GetValue(ContextProperty); }
-		    set { SetValue(ContextProperty, value); }
-	    }
-	    
-        public ContextView()
-        {
-            this.InitializeComponent();
-        }
-    }
+	public partial class ContextView : StackPanel
+	{
+		public static readonly DependencyProperty ContextProperty = DependencyProperty.Register(
+			nameof (ContextView.Context),
+			typeof (Context),
+			typeof (ContextView),
+			new PropertyMetadata(null)
+		);
+
+		public static Image DefaultPosterImage { get ; }
+			= CreateSingleColorImage(Color.Chartreuse, new Size(1024, 1024)).Result;
+
+		public Context Context
+		{
+			get { return (Context) GetValue(ContextProperty); }
+			set { SetValue(ContextProperty, value); }
+		}
+
+		public Image Poster
+		{
+			get
+			{
+				if (poster != null)
+				{
+					return poster;
+				}
+				else if (Context.PosterURL == null)
+				{
+					return DefaultPosterImage;
+				}
+				else
+				{
+					retrievePosterImage();
+					return poster!;
+				}
+			}
+		}
+
+		private Image? poster = null;
+
+		public ContextView()
+		{
+			this.InitializeComponent();
+		}
+
+		private void retrievePosterImage()
+		{
+			if (Context?.PosterURL != null)
+			{
+				poster = new Image { Source = new BitmapImage(Context.PosterURL) };
+			}
+		}
+	}
 }
