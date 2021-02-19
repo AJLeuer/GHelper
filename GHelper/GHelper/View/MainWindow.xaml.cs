fe27c18a192d8074a803d3ca8fec7d41741d9894
Adam Leuer <aleuer2@illinois.edu>
2021-02-18 23:19:46 -0800

    Removed unused/redundant code

--- GHelper/GHelper/View/MainWindow.xaml.cs
+++ GHelper/GHelper/View/MainWindow.xaml.cs
@@ -9,11 +9,9 @@ namespace GHelper.View
 {
 	public sealed partial class MainWindow : Window
 	{
-		public ObservableCollection<ApplicationViewModel>?	Applications { get; set; }
-
-		public  GHubRecord?                  CurrentDisplayedItem { get; private set; } = null;
-		private Action?                      SaveFunction         { get; set; }
-		private Action<GHubRecordViewModel>? DeleteFunction       { get; set; } 
+		public  ObservableCollection<ApplicationViewModel>? Applications   { get; set; }
+		private Action?                                     SaveFunction   { get; set; }
+		private Action<GHubRecordViewModel>?                DeleteFunction { get; set; } 
 
 
 		public MainWindow()
@@ -21,31 +19,11 @@ namespace GHelper.View
 			this.InitializeComponent();
 		}
 
-		private void ApplicationSelected(ApplicationViewModel application)
-		{
-			GHubDataDisplay.Content = RecordView.CreateViewForViewModel(application, SaveFunction!, DeleteFunction!);
-		}
-
-		private void ProfileSelected(ProfileViewModel profile)
-		{
-			GHubDataDisplay.Content = RecordView.CreateViewForViewModel(profile, SaveFunction!, DeleteFunction!);
-		}
-
 		private void HandleSelectedGHubRecordChanged(TreeView sender, TreeViewSelectionChangedEventArgs info)
 		{
-			object? selectedItem = sender.SelectedItem;
-			
-			if (selectedItem is GHubRecordViewModel gHubRecord)
+			if (sender.SelectedItem is GHubRecordViewModel gHubRecord)
 			{
-				switch (gHubRecord)
-				{
-					case ApplicationViewModel application:
-						ApplicationSelected(application);
-						break;
-					case ProfileViewModel profile:
-						ProfileSelected(profile);
-						break;
-				}	
+				GHubDataDisplay.Content = RecordView.CreateViewForViewModel(gHubRecord, SaveFunction!, DeleteFunction!);
 			}
 		}
 
