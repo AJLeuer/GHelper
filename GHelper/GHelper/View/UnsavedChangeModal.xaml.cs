f7e20ab532265c5b85df32ff136fe8c381b3905f
Adam Leuer <aleuer2@illinois.edu>
2021-02-19 14:40:06 -0800

    Created modal dialogue for unsaved changes

---
+++ GHelper/GHelper/View/UnsavedChangeModal.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace GHelper.View
+{
+    public sealed partial class UnsavedChangeModal : ContentDialog
+    {
+        public UnsavedChangeModal()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
