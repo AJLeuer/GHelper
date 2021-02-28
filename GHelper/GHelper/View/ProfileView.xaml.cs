9d92fe400a2489ddaa85ee5245390315bed3d6da
Adam Leuer <aleuer2@illinois.edu>
2021-02-28 00:41:39 -0800

    Removed unnecessary event handler for "Enter" button

--- GHelper/GHelper/View/ProfileView.xaml.cs
+++ GHelper/GHelper/View/ProfileView.xaml.cs
@@ -5,7 +5,6 @@ using GHelper.Event;
 using GHelper.ViewModel;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Input;
 
 namespace GHelper.View
 {
@@ -55,11 +54,6 @@ namespace GHelper.View
 			RecordView.ChangeName(this, sender);
 		}
 
-	    private void HandleNameChange(KeyboardAccelerator sender, KeyboardAcceleratorInvokedEventArgs eventInfo)
-	    {
-		    RecordView.ChangeName(this, eventInfo.Element);
-	    }
-	    
 	    private void ResetAppearance()
 	    {
 		    RecordViewControls.ResetAppearance();
