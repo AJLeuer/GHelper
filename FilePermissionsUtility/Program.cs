5579369219151ccee328e5bb58a4ce8bdf62fa73
Adam Leuer <aleuer2@illinois.edu>
2021-03-03 15:01:54 -0800

    Removed debug code

--- FilePermissionsUtility/Program.cs
+++ FilePermissionsUtility/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Threading;
 
 namespace FilePermissionsUtility
 {
@@ -8,18 +7,7 @@ namespace FilePermissionsUtility
     {
         static void Main()
         {
-            // todo: remove debugging code
-            ushort waitVariable = 0;
-
-            while (waitVariable < 1)
-            {
-                Console.WriteLine("Waiting for debugger to attach");
-                Thread.Sleep(TimeSpan.FromSeconds(1));
-            }
-            // end todo
-
-            //var file = new FileInfo(Environment.GetCommandLineArgs()[1]);
-            var file = new FileInfo(@"C:\ProgramData\LGHUB\depots\76775\core\applications\doom_2016_poster.png");
+            var file = new FileInfo(Environment.GetCommandLineArgs()[1]);
             FilePermissionsEditor.TakeOwnership(file);
         }
     }
